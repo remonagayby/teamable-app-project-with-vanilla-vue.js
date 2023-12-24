df78eddd6c73f7ef2e479c7e08a232b5f09f6ed1
Remon Agayby <agayby.remon.com>
2023-12-24 05:27:30 +0200

    add validator for email and new birthday field

--- README.rd
+++ README.rd
@@ -1,2 +1,6 @@
-Teamable App 1.0.0
-HTML, CSS, Vanilla Javascript
+**Teamable App 1.1.0
+- Used "Validator" library to validate the mail address updated by the user
+- Added a filed which allows the users to enter their birthday date using datepicker library
+
+**Teamable App 1.0.0
+- Create the app UI using HTML, CSS, Vanilla Javascript
--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+const datepicker = require("Datepicker.js")
+
 function handleEditProfile() {
+
+    new Datepicker('#birthday')
 
     var name = document.getElementsByTagName("b")[0].textContent
     var inputName = document.getElementsByTagName("input")[0]
@@ -12,6 +16,10 @@ function handleEditProfile() {
     var inputInterests = document.getElementsByTagName("input")[2]
     inputInterests.value = interests
 
+    var birthDay = document.getElementsByTagName("b")[3].textContent
+    var inputBirthDay = document.getElementsByTagName("input")[3]
+    inputBirthDay.value = birthDay
+
     document.getElementById("edit-view").style.display = "block"
     document.getElementById("display-view").style.display = "none"
 }
@@ -24,11 +32,18 @@ function handleUpdateProfile() {
 
     var updatedEmail = document.getElementsByTagName("input")[1].value
     var email = document.getElementsByTagName("b")[1]
-    email.textContent = updatedEmail
+    if (validator.isEmail(updatedEmail))
+        email.textContent = updatedEmail
+    else
+        alert("wrong email format")
 
     var updatedInterests = document.getElementsByTagName("input")[2].value
     var interests = document.getElementsByTagName("b")[2]
     interests.textContent = updatedInterests
+
+    var updatedBirthDay = document.getElementsByTagName("input")[3].value
+    var birthDay = document.getElementsByTagName("b")[3]
+    birthDay.textContent = updatedBirthDay
 
     document.getElementById("edit-view").style.display = "none"
     document.getElementById("display-view").style.display = "block"
